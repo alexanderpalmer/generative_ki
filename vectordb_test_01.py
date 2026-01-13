73f8d15b8392fead4f78b8c9943808a27e81ed49
Alexander Palmer <apalmer@zbw.ch>
2026-01-13 16:50:13 +0100

    Add initial test script for loading text documents using Langchain Community's TextLoader
    - Created vectordb_test_01.py to test loading a text file.
    - Implemented functionality to load "HoundOfBaskerville.txt" from the data directory.
    - Included print statement to display loaded document content.

---
+++ vectordb_test_01.py
@@ -0,0 +1,13 @@
+import os
+from langchain_community.document_loaders import TextLoader
+
+# Get the current working directory
+file_path = os.path.abspath(__file__)
+current_dir = os.path.dirname(file_path)
+
+file_path = os.path.join(current_dir, "data", "HoundOfBaskerville.txt")
+
+text_loader = TextLoader(file_path=file_path, encoding="utf-8")
+doc = text_loader.load()
+print(doc)
+
