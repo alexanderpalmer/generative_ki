3fc71bd0590c9019171567ae7c8287852d362a92
Alexander Palmer <apalmer@zbw.ch>
2026-01-16 09:02:23 +0100

    Refactor output display in vectordb_test_01.py to show document metadata and content separately

--- vectordb_test_01.py
+++ vectordb_test_01.py
@@ -9,5 +9,5 @@ file_path = os.path.join(current_dir, "data", "HoundOfBaskerville.txt")
 
 text_loader = TextLoader(file_path=file_path, encoding="utf-8")
 doc = text_loader.load()
-print(doc)
-
+print(doc[0].metadata)
+print(doc[0].page_content)
